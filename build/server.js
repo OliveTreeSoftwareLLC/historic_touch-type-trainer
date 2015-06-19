5e8fe43dae83e3b8755a3ab7dcabad11bcbf18be
StillLearnin <stilllearnin@thettpost.com>
2015-06-18 23:45:19 -0400

    basic build working

--- build/server.js
+++ build/server.js
@@ -1,11 +1,36 @@
+var bodyParser = require("body-parser");
 var express = require('express');
+var watch = require('node-watch');
+var ncp = require('ncp').ncp;
+var path = require("path");
 var app = express();
 
 app.use(express.static('assets'));
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+ncp.limit = 16;
+
+watch('app', function (filename) {
+    console.log(filename, ' changed.');
+    var destName = path.join(__dirname, '..', 'public', filename.substring(3));
+    ncp(filename, destName, function (err) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log('Finished copying ' + filename + '!');
+    });
+});
+
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.sendfile("index.html");
+});
+app.post('/login', function (req, res) {
+    var user_name = req.body.user;
+    var password = req.body.password;
+    console.log("User name = " + user_name + ", password is " + password);
+    res.end("yes");
 });
 
 var server = app.listen(4000, function () {
@@ -13,6 +38,6 @@ var server = app.listen(4000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Touch-type-trainer server listening at http://%s:%s', host, port);
+    console.log('Touch-type-trainer server listening at http://localhost:%s', port);
 
 });
