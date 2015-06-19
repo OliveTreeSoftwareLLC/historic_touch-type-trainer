99ae559c78da67dcce2aa41dd06525aa2bcd1a09
StillLearnin <stilllearnin@thettpost.com>
2015-06-18 22:42:57 -0400

    let's start with the basics

--- build/server.js
+++ build/server.js
@@ -1,54 +1,18 @@
 var express = require('express');
-var compression = require('compression');
-var path = require('path');
-var http = require('http');
-var log = require('npmlog');
-var WebSocket = require('faye-websocket');
-var without = require('lodash/array/without');
+var app = express();
 
-var output = path.join(path.dirname(__dirname), 'output', 'dist');
-var example = path.join(path.dirname(__dirname), 'output', 'example');
+app.use(express.static('assets'));
+app.use(express.static('public'));
 
-module.exports = Server;
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
 
-function Server() {
-    this.app = express();
-    this.server = http.createServer(this.app);
-    this.server.on('upgrade', this.upgrade.bind(this));
+var server = app.listen(4000, function () {
 
-    this.app.use(compression());
+    var host = server.address().address;
+    var port = server.address().port;
 
-    this.app.get('/output/', function(req, resp) {
-        resp.redirect('/');
-    });
+    console.log('Touch-type-trainer server listening at http://%s:%s', host, port);
 
-    this.app.use(express.static(output));
-    this.app.use('/example', express.static(example));
-
-    this.connections = [];
-
-    var port = process.env.PORT || process.env.CLIENT_BUILD_PORT || 8000;
-    log.verbose('server', 'starting on port %s', port);
-    this.server.listen(port);
-}
-
-Server.prototype.reload = function() {
-    this.connections.forEach(function(c) {
-        c.send('reload', function() { });
-    });
-};
-
-Server.prototype.upgrade = function(req, socket, body) {
-    if (WebSocket.isWebSocket(req))
-        this.connect(new WebSocket(req, socket, body));
-};
-
-Server.prototype.connect = function(c) {
-    log.verbose('server', 'client connected to live reload');
-
-    this.connections.push(c);
-    c.on('close', function() {
-        log.verbose('client disconnected from live reload');
-        this.connections = without(this.connections, c);
-    }.bind(this));
-};
+});
