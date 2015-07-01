0c7281956de8026d23f9b09265cd41738a8685df
StillLearnin <stilllearnin@thettpost.com>
2015-06-30 22:25:34 -0400

    Render footer correctly when not logged in.

--- app/footer/footer.jsx
+++ app/footer/footer.jsx
@@ -2,13 +2,37 @@
 'use strict'
 require("./footer.css")
 var React = require('react');
+var AuthStore = require('../stores/AuthenticationStore');
 var Keyboard = require('../keyboard/keyboard');
 
 module.exports = React.createClass({
 
+    getInitialState() {
+        return { isLoggedIn: AuthStore.isLoggedIn() };
+    },
+
+    componentDidMount() {
+        AuthStore.addChangeListener(this._onChange);
+    },
+
+    componentWillUnmount() {
+        AuthStore.removeChangeListener(this._onChange);
+    },
+
+    _onChange() {
+        this.setState({ isLoggedIn: AuthStore.isLoggedIn() });
+    },
+
     render() {
         return <div className='footer'>
-            <Keyboard/>
+            {this.renderContents()}
         </div>;
+    },
+
+    renderContents() {
+        if (!this.state.isLoggedIn)
+            return;
+
+        return <Keyboard/>
     }
 });
