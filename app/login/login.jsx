617da9fe9ba20e0a013d6d15ea7fc8fa5525a237
StillLearnin <stilllearnin@thettpost.com>
2015-08-17 19:30:46 -0400

    more login work.

--- app/login/login.jsx
+++ app/login/login.jsx
@@ -2,16 +2,44 @@
 'use strict'
 require("./login.css")
 var React = require('react');
+var AuthStore = require('../stores/AuthenticationStore');
 var AppDispatcher = require('../dispatchers/AppDispatcher');
 
 module.exports = React.createClass({
 
+    getInitialState() {
+        return { isWaiting: AuthStore.isWaitingOnServer(),
+            errorData: AuthStore.getLoginError()
+        };
+    },
+
     componentDidMount() {
         document.getElementById("Username").focus();
+        AuthStore.addChangeListener(this.handleAuthChange);
+    },
+
+    componentWillUnmount() {
+        AuthStore.removeChangeListener(this.handleAuthChange);
+    },
+
+    handleAuthChange() {
+        this.setState({
+            isWaiting: AuthStore.isWaitingOnServer(),
+            errorData: AuthStore.getLoginError()
+        });
     },
 
     render() {
+        if (this.state.isWaiting) {
+            return <form className='login'>
+                <div>
+                    Please Wait....
+                </div>
+            </form>;
+        }
+
         return <form className='login'>
+            {this.renderErrorMsg()}
             Enter your credentials
             <div>
                 <input id="Username" placeholder='Username'/>
@@ -23,6 +51,15 @@ module.exports = React.createClass({
                 <button type='submit' onClick={this.login}>Login</button>
             </div>
         </form>;
+    },
+
+    renderErrorMsg() {
+        if (!this.state.errorData)
+            return null;
+
+        return <div className='login-error'>
+            {this.state.errorData}
+        </div>
     },
 
     login() {
